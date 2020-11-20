ad088a09f55d5fda74ed3e76e741aeb44e873cca
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-11-20 18:12:13 -0300

    Exercicio de Seleção de Elementos 1 finalizado

---
+++ DOM-1/main.js
@@ -0,0 +1,12 @@
+const h1 = location.href;
+console.log(h1);
+
+const elementoAtivo = document.querySelector('.ativo');
+console.log(elementoAtivo);
+
+const idioma = navigator.language;
+console.log(idioma);
+
+const largura = innerWidth;
+console.log(largura);
+
