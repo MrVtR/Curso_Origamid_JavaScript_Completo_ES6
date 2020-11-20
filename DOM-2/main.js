05b74bb9fe05840e0a280a360d663b35b84c0267
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-11-20 18:50:27 -0300

    Exercicio Seleção de Elementos 2 ajustado

--- DOM-2/main.js
+++ DOM-2/main.js
@@ -1,14 +1,14 @@
 const img = document.querySelectorAll('img');
 console.log(img);
 
-const imagens = querySelectorAll('img[src^="img/imagem"]');
+const imagens = document.querySelectorAll('img[src^="img/imagem"]');
 console.log(imagens);
 
 const linkInterno = document.querySelectorAll('[href^="#"]');
 console.log(linkInterno);
 
-const h2 = document.querySelector('.seletor h2');
-console.log(h2);
+const seletor = document.querySelector('.seletor h2');
+console.log(seletor);
 
 const p = document.querySelectorAll('p');
 console.log(p[p.length-1]);
