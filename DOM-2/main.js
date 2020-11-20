5320b1cca1e33a0166236edcd3b53e932073193d
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-11-20 18:13:16 -0300

    Exercicio de Seleção de Elementos 2 finalizado

---
+++ DOM-2/main.js
@@ -0,0 +1,14 @@
+const img = document.querySelectorAll('img');
+console.log(img);
+
+const imagens = querySelectorAll('img[src^="img/imagem"]');
+console.log(imagens);
+
+const linkInterno = document.querySelectorAll('[href^="#"]');
+console.log(linkInterno);
+
+const h2 = document.querySelector('.seletor h2');
+console.log(h2);
+
+const p = document.querySelectorAll('p');
+console.log(p[p.length-1]);
